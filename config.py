2f3f0465c1a9e18ec6db31bab41ee72a349b9017
simonmachadoo234-a11y <simonmachadoo234@gmail.com>
2026-07-16 00:38:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,11 @@ load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-MINECRAFT_IP = os.getenv("MINECRAFT_IP")
-
-MINECRAFT_PORT = int(
-    os.getenv("MINECRAFT_PORT", 25565)
-)
-
 STATUS_CHANNEL = int(
-    os.getenv("STATUS_CHANNEL")
+    os.getenv("STATUS_CHANNEL", "0")
 )
+
+
+# Minecraft
+MINECRAFT_IP = "Krypt_server.aternos.me"
+MINECRAFT_PORT = 43787

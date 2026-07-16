8ca4863c99dbbe57b25dd6ebddca3e4f7d8d7aef
simonmachadoo234-a11y <simonmachadoo234@gmail.com>
2026-07-15 21:39:38 -0300

    Update minecraft.py

--- minecraft.py
+++ minecraft.py
@@ -1,5 +1,6 @@
 from mcstatus import JavaServer
-from config import MINECRAFT_IP, MINECRAFT_PORT
+from mcrcon import MCRcon
+from config import MINECRAFT_IP, MINECRAFT_PORT, RCON_PASSWORD
 
 
 server = JavaServer.lookup(
@@ -10,18 +11,40 @@ server = JavaServer.lookup(
 def get_status():
 
     try:
-        server.ping()
 
         status = server.status()
 
         return {
             "online": True,
             "players": f"{status.players.online}/{status.players.max}",
-            "version": status.version.name
+            "version": status.version.name,
+            "ping": round(status.latency)
         }
+
 
     except:
 
         return {
             "online": False
         }
+
+
+
+def send_command(command):
+
+    try:
+
+        with MCRcon(
+            MINECRAFT_IP,
+            RCON_PASSWORD,
+            port=25575
+        ) as rcon:
+
+            response = rcon.command(command)
+
+            return response
+
+
+    except Exception as e:
+
+        return str(e)
